7581fac64f88fd75f8c057fa8573813b06c7b4d3
Leon3414 <57513761+Leon3414@users.noreply.github.com>
2021-07-10 15:51:26 -0500

    Update Radixsort.java

--- Radixsort.java
+++ Radixsort.java
@@ -1,81 +1,110 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package proyectoordenamientoexterno;
 
-/**
- *
- * @author Leo
- */
+//package proyectoordenamientoexterno;
+
+
 import java.io.*;
 import java.util.*;
+import java.util.Scanner;
  
 public class Radixsort {
+    int c[] = new int[500];
+    Scanner opcion = new Scanner(System.in);
+    
     public int getMax(int arr[], int n)
     {
-        int mx = arr[0];
-        for (int i = 1; i < n; i++)
-            if (arr[i] > mx)
-                mx = arr[i];
-        return mx;
+        int max = arr[0];
+        for (int i=1; i<n; i++){
+            if (arr[i] > max){
+                max = arr[i];
+            }
+        }    
+        return max;
     }
  
     
-    public void countSort(int arr[], int n, int exp)
-    {
+    public void countAscendente(int arr[], int n, int exp){
         int output[] = new int[n]; 
         int i;
         int count[] = new int[10];
         Arrays.fill(count, 0);
- 
-        for (i = 0; i < n; i++)
+       
+        for(i=0; i<n; i++){
             count[(arr[i] / exp) % 10]++;
- 
-        for (i = 1; i < 10; i++)
+        }
+        
+        for(i=1; i<10; i++){
             count[i] += count[i - 1];
- 
-        for (i = n - 1; i >= 0; i--) {
-            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-            count[(arr[i] / exp) % 10]--;
+        }
+        
+        for(i=n-1; i>=0; i--) {
+            output[--count[(arr[i] / exp) % 10] ] = arr[i];
         }
 
-        for (i = 0; i < n; i++)
+        for(i =0; i<n; i++){
             arr[i] = output[i];
+        }
+    }
+
+    public void countDescendente(int arr[], int n, int exp){
+        int output[] = new int[n]; 
+        int i;
+        int count[] = new int[10];
+        Arrays.fill(count, 0);
+       
+        for(i=0; i<n; i++){
+            count[9-(arr[i] / exp) % 10]++;
+        }
+        
+        for(i=1; i<10; i++){
+            count[i] += count[i - 1];
+        }
+        
+        for(i=n-1; i>=0; i--) {
+            output[--count[9-(arr[i] / exp) % 10] ] = arr[i];
+        }
+
+        for(i =0; i<n; i++){
+            arr[i] = output[i];
+        }
     }
     
     public void radix(String nombrearchivo)throws FileNotFoundException, IOException{
         FileReader lectura = new FileReader(nombrearchivo);
         BufferedReader buff = new BufferedReader(lectura);
-        ArrayList<String[]> aux = new ArrayList<>();
-        int c[] = {};
-            for(int i = 0; i<500; i++){
-                String contenido = buff.readLine();
-                aux.add(contenido.split(",")); 
-            }
-            for(int x = 0; x<500; x++){
-                String mystring = String.join(" ", aux.get(x));               
-                c[x]= Integer.parseInt(mystring);
-            }
-        radixsort(c, 500);    
-        buff.close();
+        String linea = buff.readLine();
+        String[] key = linea.split(",");
         
+        for(int i=0; i<key.length; i++){
+            c[i]=Integer.parseInt(key[i]);
+        }
+        radixsort(c, 500);
     }
     
-    public void radixsort(int arr[], int n)
-    {
-      
-        int m = getMax(arr, n);
+    public void radixsort(int arr[], int n)throws IOException{
+        int x = getMax(arr, n);
+        System.out.println("Seleccione en que orden desea ordenar el documento\n1)Ascendente\n2)Descendente");
+        int y = opcion.nextInt();
+        switch(y){
+            case 1:
+                for(int exp=1; x/exp>0; exp*=10){
+                    countAscendente(arr, n, exp);
+                }
+                break;
+            case 2:
+                for(int exp=1; x/exp>0; exp*=10){
+                    countDescendente(arr, n, exp);
+                }
+                break;
+            default: 
+                System.out.println("Opcion no valida");
+        }
+        FileWriter archivo = new FileWriter("ArchivoOrdenado.txt");
+        int cantidad = 500;
         
-        for (int exp = 1; m / exp > 0; exp *= 10)
-            countSort(arr, n, exp);
-    }
- 
-    public void print(int arr[], int n)
-    {
-        for (int i = 0; i < n; i++)
-            System.out.print(arr[i] + " ");
-    }
-    
+        for(int i=0; i<cantidad; i++){
+            archivo.write(c[i]+",");
+        }
+        archivo.close();
+        System.out.println("Archivo Ordenado Correctamente");
+    }  
 }
